1d908c7573498c0b8dabdc57e17fcd27844408b1
Akzam Khamitov <makoron3002@gmail.com>
2023-09-27 00:57:02 +0600

    Added two gradle modules, one for postgreSqlCrud, the other for mongoDbCrud. Took out dependency versions in gradle.properties. Configured application.yml for both microservices. Wrote docker-compose for Mongodb and PostgreSQL

---
+++ postgreSqlCrud/src/main/java/com/akzam/postgreSqlCrud/PostgreSqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.akzam.postgreSqlCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgreSqlCrudApplication.class, args);
+    }
+
+}
